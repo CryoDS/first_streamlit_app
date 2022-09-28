92af8fadc6d953303e4f512e18282c5edd26c453
CryoDS <113437001+CryoDS@users.noreply.github.com>
2022-09-28 16:38:45 +0300

    Update streamlit_app.py
    added breakfast menu header and 3 repeating items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Test')
+streamlit.text('Omega 3 & Blueberry Otameal')
+streamlit.text('Omega 3 & Blueberry Otameal')
+streamlit.text('Omega 3 & Blueberry Otameal')
+streamlit.text('Omega 3 & Blueberry Otameal')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
